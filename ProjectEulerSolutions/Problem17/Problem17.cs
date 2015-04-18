ab16efd9badfc5ddb835573e6b5a16fb1fecd82b
Kyle McNutt <Kyle.McNutt@disney.com>
2015-04-17 17:49:34 -0700

    Problem 17, 18 solution

---
+++ ProjectEulerSolutions/Problem17/Problem17.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+namespace Problem17
+{
+    class Problem17
+    {
+        static void Main()
+        {
+            // If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+            //
+            // If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+            //
+            //
+            // NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out 
+            // numbers is in compliance with British usage.
+            //
+            // So this problem is actually rather difficult. So I've had to make an entire OTHER class for this problem. This class is going to act as an Extension so I can convert a number into a words. Go ahead
+            // and jump over there and read the comments so you can get an idea of what it's doing.
+            //
+            // With the ConvertToWordsExtension in place it's actually trivial to solve this problem...
+
+            Console.WriteLine("The number of letters used in the numbers from 1 to 1000 are: "+1.To(1000)
+                    .Select(number => number.ConvertToWords().ToCharArray().Count(char.IsLetter))
+                    .Sum());
+            Console.ReadLine();
+        }
+    }
+
+    // Basically how this extension works is the following: 
+    // 1. Let n be the number to process
+    // 2. For numbers smaller than 0, combine "Negative" with the result of calling the algorithm on the absolute value of n
+    // 3. For n between 0 and 20, look up the result in the word dictionary
+    // 4. For n between 20 and 100, calculate the number of 10s and number of units; look up the name for the number of tens; if there are any units, look up the appropriate name and combine the two results
+    // with a hyphen
+    // 5. For n between 100 and 1000, calculate the number of 100s and the remainder; look up the name of the number of hundreds; if there is any remainder, recurse to get its wordy representation, then combine
+    // it with result for the hundreds using "and" to join the two parts
+    // 6. For n bigger than 1000: decide which is the biggest named number (million, billion, etc.) that divides into n. Calculate the number of that base unit and remainder. Recurse to convert the number of base
+    // units into words, and recurse to convert the remainder into words. Combine the two parts using ","
+    public static class ConvertToWordsExtension
+    {
+        private static Dictionary<long, string> _wordDictionary;
+        private const int OneThousand = 1000;
+        private const long OneMillion = 1000000;
+        private const long OneBillion = 1000000000;
+        private const long OneTrillion = 1000000000000;
+        private const long OneQuadrillion = 1000000000000000;
+        private const long OneQuintillion = 1000000000000000000;
+
+        // Converts a number to its English representation in words.
+        public static string ConvertToWords(this long number)
+        {
+            EnsureWordDictionaryInitialised();
+
+            if (number == long.MinValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return ConvertToWordsCore(number);
+        }
+
+        // Converts a number to its English representation in words.
+        public static string ConvertToWords(this int number)
+        {
+            return ConvertToWords((long)number);
+        }
+
+        private static Dictionary<long, string> CreateWordDictionary()
+        {
+            return new Dictionary<long, string>
+            {
+                {0, "zero"},
+                {1, "one"},
+                {2, "two"},
+                {3, "three"},
+                {4, "four"},
+                {5, "five"},
+                {6, "six"},
+                {7, "seven"},
+                {8, "eight"},
+                {9, "nine"},
+                {10, "ten"},
+                {11, "eleven"},
+                {12, "twelve"},
+                {13, "thirteen"},
+                {14, "fourteen"},
+                {15, "fifteen"},
+                {16, "sixteen"},
+                {17, "seventeen"},
+                {18, "eighteen"},
+                {19, "nineteen"},
+                {20, "twenty"},
+                {30, "thirty"},
+                {40, "forty"},
+                {50, "fifty"},
+                {60, "sixty"},
+                {70, "seventy"},
+                {80, "eighty"},
+                {90, "ninety"},
+                {100, "hundred"},
+                {OneThousand, "thousand"},
+                {OneMillion, "million"},
+                {OneBillion, "billion"},
+                {OneTrillion, "trillion"},
+                {OneQuadrillion, "quadrillion"},
+                {OneQuintillion, "quintillion"}
+            };
+        } 
+
+        // 
+        private static void EnsureWordDictionaryInitialised()
+        {
+            // Ensuer thread-safety when caching our word dictionary. Note: This doesn't prevent two copies of the word dictionary being initialised - but that doesn't matter; only one would be cached, the
+            // other garbage collected.
+            if (_wordDictionary == null)
+            {
+                var dictionary = CreateWordDictionary();
+                Interlocked.CompareExchange(ref _wordDictionary, dictionary, null);
+            }
+        }
+
+        private static string ConvertToWordsCore(long number)
+        {
+            return
+             number < 0 ? "negative " + ConvertToWordsCore(Math.Abs(number))
+             : 0 <= number && number < 20 ? _wordDictionary[number]
+             : 20 <= number && number < 100 ? ProcessTens(number, _wordDictionary)
+             : 100 <= number && number < OneThousand ? ProcessHundreds(number, _wordDictionary)
+             : OneThousand <= number && number < OneMillion ? ProcessLargeNumber(number, OneThousand, _wordDictionary)
+             : OneMillion <= number && number < OneBillion ? ProcessLargeNumber(number, OneMillion, _wordDictionary)
+             : OneBillion <= number && number < OneTrillion ? ProcessLargeNumber(number, OneBillion, _wordDictionary)
+             : OneTrillion <= number && number < OneQuadrillion ? ProcessLargeNumber(number, OneTrillion, _wordDictionary)
+             : OneQuadrillion <= number && number < OneQuintillion ? ProcessLargeNumber(number, OneQuadrillion, _wordDictionary)
+             : ProcessLargeNumber(number, OneQuintillion, _wordDictionary); // long.Max value is just over nine quintillion
+        }
+
+        private static string ProcessLargeNumber(long number, long baseUnit, Dictionary<long, string> wordDictionary)
+        {
+            // split the number into number of baseUnits (thousands, millions, etc.) and the remainder
+            var numberOfBaseUnits = number/baseUnit;
+            var remainder = number%baseUnit;
+
+            // apply ConvertToWordsCore to represent the number of baseUnits as words
+            string conversion = ConvertToWordsCore(numberOfBaseUnits) + " " + wordDictionary[baseUnit];
+            // recurse for any remainder
+            conversion += remainder <= 0 ? "" : (remainder < 100 ? " and " : ", ") + ConvertToWordsCore(remainder);
+            return conversion;
+        }
+
+        private static string ProcessHundreds(long number, Dictionary<long, string> wordDictionary)
+        {
+            var hundreds = number/100;
+            var remainder = number%100;
+            string conversion = wordDictionary[hundreds] + " " + wordDictionary[100];
+            conversion += remainder > 0 ? " and " + ConvertToWordsCore(remainder) : "";
+            return conversion;
+        }
+
+        private static string ProcessTens(long number, Dictionary<long, string> wordDictionary)
+        {
+            Debug.Assert(0 <= number && number < 100);
+
+            // Split the number into the number of tens and the number of units, so that words for both can be looked up independantly
+            var tens = (number/10)*10;
+            var units = number%10;
+            string conversion = wordDictionary[tens];
+            conversion += units > 0 ? "-" + wordDictionary[units] : "";
+            return conversion;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<long> To(this int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
---
+++ ProjectEulerSolutions/Problem18/Problem18.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+namespace Problem18
+{
+    class Problem18
+    {
+        static void Main()
+        {
+            // By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+            //
+            //                                                                     3
+            //                                                                    7 4
+            //                                                                   2 4 6
+            //                                                                  8 5 9 3
+            //
+            // That is, 3 + 7 + 4 + 9 = 23.
+            //
+            // Find the maximum total from top to bottom of the triangle below:
+            //
+            //                                                                      75
+            //                                                                     95 64
+            //                                                                    17 47 82
+            //                                                                   18 35 87 10
+            //                                                                  20 04 82 47 65
+            //                                                                 19 01 23 75 03 34
+            //                                                                88 02 77 73 07 63 67
+            //                                                               99 65 04 28 06 16 70 92
+            //                                                              41 41 26 56 83 40 80 70 33
+            //                                                             41 48 72 33 47 32 37 16 94 29
+            //                                                            53 71 44 65 25 43 91 52 97 51 14
+            //                                                           70 11 33 28 77 73 17 78 39 68 17 57
+            //                                                          91 71 52 38 17 14 91 43 58 50 27 29 48
+            //                                                         63 66 04 68 89 53 67 30 73 16 69 87 40 31
+            //                                                        04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+            //
+            // NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot 
+            // be solved by brute force, and requires a clever method! ;o)
+            // 
+            // I thought I should explain how I decided to store the triange of data first. Basically the data will look like:
+            //
+            //                                                                       3 0 0 0
+            //                                                                       7 4 0 0
+            //                                                                       2 4 6 0
+            //                                                                       8 5 9 3
+            //
+            // in terms of the triangle going right increases the index by one, and going left keeps the index, and the triangle is padded with zeros.
+            //
+            // I also didn't want to input all this data manually, and I like things I can reuse, so I wrote a readInput function which reads the data from a input.txt file that you can find the debug folder of this
+            // project.
+            // 
+            // The given problem is a classical exmaple of dynamic programming, and it really works well fo rit. The methodology is a bit more complex than the brute force solution, but I will take a shot at explaning
+            // it anyway. It's probably easiest with an example so I'm going to use the four line example at the top of the question.
+            //
+            // Standing at the top of the triange we have to choose between going left and right. In order to make the optimal choice (which maximizes the sum), we would have to know how large a sum we can get if we
+            // go either way. So in order to answer the question we would basically have to solve smaller versions of the same problem, imagine two triangles around the numbers one row under the top row.
+            //
+            // We can break each of the sub-problems down in a similar way, and we can continue to do so until we reach a sub-problem at the bottom line consisting of only one number, then the question becomes trivial
+            // to answer, since the answer is the number it self. Once that question is answered we can move up one row, and answer the question posed there with a solution which is a + max(b,c).
+            //
+            // Once we know the answer to all 3 sub-problems on the next to last line, we can move up and answer the posed sub-problems by the same formula already applied. And we can continue to so until we reach
+            // the original question of whether to go left or right.
+            //
+            // If we break down the problem into sub-problems we can see that breaking the left-three-row triangle into two and breaking the right-three-row triangle into two would yield us 4 triangles, or sub-problems.
+            // However the sub-problem in the overlapping part is identical. In this problem solving the sub-problem yields the same result no matter how we reached it. This is fairly easy to see in our example. When
+            // a problem has this property it is said to have optimal sub-structure. Since we have a problem with optimal sub structure we only have to solve three sub-problems in the next to bottom line, and therefore 
+            // the dynamic programming solution is effective.
+            //
+            // It can be proven that the problem has an optimal sub-structure. However, I won't go into the details of that here, but leave that as an open end you can pick up and explore.
+            //
+            // If we want to solve the small problem with brute force, we would need to test all 8 paths, each resulting in 3 additions, in total 24 additions.
+            //
+            // If we use dynamic programming, the first iteration would require 3 maximum comparison operations and 3 additions. The next line would require 2 maximum comparison operations and 2 additions, and the first
+            // line would require one of each. So a total of 6 maximum comparison operations and 6 additions.
+            //
+            // For small problems this saving is small if any at all, but for a problem with 15 lines, solving the first iteration and brute forcing from there would reduce the number of brute force additions
+            // from 15*2^14 to 14*2^13 a saving of approximately 131,000 fewer additions, at the cost of 15 additions and 15 maximum comparison operations. That is a pretty good improvment.
+            //
+            // We can make a short-cut with the algorithm, as we don't have to break the problem into sub-problems, but can start from the bottom and work the way up through the triangle until we reach the top and
+            // the algorithm spits out a number.
+            //
+            // We start with a triangle that looks like:
+            //                                                                         3
+            //                                                                        7 4
+            //                                                                       2 4 6
+            //                                                                      8 5 9 3
+            //
+            // Applying the algorithm to the small problem we will need three iterations. The first iteration we apply the rule a + max(b,c) which creates a new triangle which looks as:
+            //
+            //                                                                         3
+            //                                                                        7 4
+            //                                                                      10 13 15
+            //
+            // Making the second iteration of the algorithm makes the triangle look:
+            //                                                                         3
+            //                                                                      20  19
+            //
+            // And if we run the algorithm once more, the triangle collapses to one number - 23- which is the answer to the small example.
+            //
+            // The algorithm looks a little something like this..
+
+            string filename = "input.txt";
+            int[,] inputTriangle = ReadInput(filename);
+            int lines = inputTriangle.GetLength(0);
+            for (int i = lines - 2; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    inputTriangle[i, j] += Math.Max(inputTriangle[i + 1, j], inputTriangle[i + 1, j + 1]);
+                }
+            }
+
+            Console.WriteLine("The largest sum through the triangle is: "+inputTriangle[0,0]);
+            Console.ReadLine();
+        }
+
+        public static int[,] ReadInput(string filename)
+        {
+            string line;
+            int lines = 0;
+
+            StreamReader r = new StreamReader(filename);
+            while ((r.ReadLine()) != null)
+            {
+                lines++;
+            }
+
+            int[,] inputTriangle = new int[lines, lines];
+            r.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            int j = 0;
+            while ((line = r.ReadLine()) != null)
+            {
+                var linePieces = line.Split(' ');
+                for (int i = 0; i < linePieces.Length; i++)
+                {
+                    inputTriangle[j, i] = int.Parse(linePieces[i]);
+                }
+                j++;
+            }
+            r.Close();
+            return inputTriangle;
+        }
+    }
+}
