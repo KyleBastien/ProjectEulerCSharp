1cc77c7ed8e9cc7a7c77ca6c6f2a4a5659f8118f
Kyle McNutt <Kyle.McNutt@disney.com>
2015-04-21 08:33:09 -0700

    Problem 20 solution and 19 clean up

---
+++ ProjectEulerSolutions/Problem20/Problem20.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+namespace Problem20
+{
+    class Problem20
+    {
+        static void Main()
+        {
+            // n! means n × (n − 1) × ... × 3 × 2 × 1
+            //
+            // For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+            // and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+            //
+            // Find the sum of the digits in the number 100!
+            //
+            // 100! is a VERY large number, but similar to Problem 13 or Problem 16 this is trivialized by the fact that we have access to the BigNumber class since .NET 4.0.
+            //
+            // This solution is actually very similar to Problem 16.
+
+            int sum = 0;
+            BigInteger fac = 1;
+
+            for (int i = 2; i <= 100; i++)
+            {
+                fac *= i;
+            }
+
+            while (fac > 0)
+            {
+                sum += (int) (fac%10);
+                fac /= 10;
+            }
+
+            Console.WriteLine("The sum of digits in 100! is: "+sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProjectEulerSolutions/Problem20/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Problem20")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Disney Interactive")]
+[assembly: AssemblyProduct("Problem20")]
+[assembly: AssemblyCopyright("Copyright © Disney Interactive 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fdbc33f2-2138-4b39-877b-636f703ef739")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
